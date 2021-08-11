3c9e68f125429b09e46093db3215dda6275a76fe
YM <yehialisation@yahoo.com>
2021-08-11 11:56:25 +0200

    First Commit

---
+++ init_myratings.m
@@ -0,0 +1,20 @@
+function my_ratings = init_myratings()
+%Fill my_ratings as you like
+%Find MovieID in movie_ids.txt or movieList variable in MATLAB
+my_ratings = zeros(1682, 1);
+my_ratings(1) = 4;
+my_ratings(11) = 5; 
+my_ratings(12)= 3;
+my_ratings(23) = 5;
+my_ratings(55)= 5;
+my_ratings(56)= 5;
+my_ratings(64) = 3;
+my_ratings(71) = 4;
+my_ratings(95) = 4;
+my_ratings(127)= 5;
+my_ratings(201)= 1;
+my_ratings(234)= 1;
+my_ratings(298)= 2;
+my_ratings(682)= 1;
+my_ratings(89)= 2;
+end
